50d379f99e731c4b71414928961a17523ca09c34
farouk daadaa <farouk.daadaa@esprit.tn>
2026-05-17 15:56:39 +0100

    init project structure with spring boot backend

---
+++ backend/src/main/java/com/nomow/company_screener/CompanyScreenerApplication.java
@@ -0,0 +1,13 @@
+package com.nomow.company_screener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyScreenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyScreenerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/nomow/company_screener/CompanyScreenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nomow.company_screener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyScreenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
